06712f6ded24d9f4cdee3576a0b457a2efaa0662
excellentservantacademy <excellentservantacademy@gmail.com>
2025-09-29 14:48:38 +0200

    firct lines of codes ina ccount file

---
+++ account.php
@@ -0,0 +1,51 @@
+<?php 
+
+include 'core/init.php';
+  
+$user_id = $_SESSION['user_id'];
+
+$user = User::getData($user_id);
+$who_users = Follow::whoToFollow($user_id);
+$notify_count = User::CountNotification($user_id);
+
+if (User::checkLogIn() === false) 
+header('location: index.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Settings | Twitter Clone</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/all.min.css">
+
+    <!-- time function to force css file to reload -->
+    
+    <link rel="stylesheet" href="assets/css/profile_style.css?v=<?php echo time(); ?>">
+    <link rel="shortcut icon" type="image/png" href="assets/images/twitter.svg"> 
+   
+</head>
+<body>
+         <div id="mine">
+    <div class="wrapper-left">
+        <div class="sidebar-left">
+          <div class="grid-sidebar" style="margin-top: 12px">
+            <div class="icon-sidebar-align">
+              <img src="<?php echo BASE_URL . "/assets/images/twitter-logo.png"; ?>" alt="" height="30px" width="30px" />
+            </div>
+          </div>
+
+          <a href="home.php">
+          <div class="grid-sidebar bg-active" style="margin-top: 12px">
+            <div class="icon-sidebar-align">
+              <img src="<?php echo BASE_URL . "/includes/icons/tweethome.png"; ?>" alt="" height="26.25px" width="26.25px" />
+            </div>
+            <div class="wrapper-left-elements">
+              <a href="home.php" style="margin-top: 4px;"><strong>Home</strong></a>
+            </div>
+          </div>
+          </a>
+  
