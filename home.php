b624461dd5a53b97c7fe932132e45ef8247aec2d
excellentservantacademy <excellentservantacademy@gmail.com>
2025-09-30 23:12:36 +0200

    edd of home page

--- home.php
+++ home.php
@@ -272,4 +272,105 @@
                   </div>
             
                 </div>
+                 </div>
+            
+                </div>
                 
+                
+              </div>
+            </div>
+            <!-- <div class="mt-icon-settings">
+              <img src="https://i.ibb.co/W5T9ycN/settings.png" alt="" />
+            </div> -->
+          </div>
+          <div class="box-fixed" id="box-fixed"></div>
+            
+          <?php  include 'includes/tweets.php'; ?>
+
+        </div>
+
+
+        <div class="wrapper-right">
+            <div style="width: 90%;" class="container">
+
+          <div class="input-group py-2 m-auto pr-5 position-relative">
+
+          <i id="icon-search" class="fas fa-search tryy"></i>
+          <input type="text" class="form-control search-input"  placeholder="Search Twitter">
+          <div class="search-result">
+
+
+          </div>
+          </div>
+          </div>
+
+
+       
+
+            
+          <div class="box-share">
+            <p class="txt-share"><strong>Who to follow</strong></p>
+            <?php 
+            foreach($who_users as $user) { 
+              //  $u = User::getData($user->user_id);
+               $user_follow = Follow::isUserFollow($user_id , $user->id) ;
+               ?>
+          <div class="grid-share">
+          <a style="position: relative; z-index:5; color:black" href="<?php echo $user->username;  ?>">
+                      <img
+                        src="assets/images/users/<?php echo $user->img; ?>"
+                        alt=""
+                        class="img-share"
+                      />
+                    </a>
+                    <div>
+                      <p>
+                      <a style="position: relative; z-index:5; color:black" href="<?php echo $user->username;  ?>">  
+                      <strong><?php echo $user->name; ?></strong>
+                      </a>
+                    </p>
+                      <p class="username">@<?php echo $user->username; ?>
+                      <?php if (Follow::FollowsYou($user->id , $user_id)) { ?>
+                  <span class="ml-1 follows-you">Follows You</span></p>
+                  <?php } ?></p>
+                    </div>
+                    <div>
+                      <button class="follow-btn follow-btn-m 
+                      <?= $user_follow ? 'following' : 'follow' ?>"
+                      data-follow="<?php echo $user->id; ?>"
+                      data-user="<?php echo $user_id; ?>"
+                      data-profile="<?php echo $u_id; ?>"
+                      style="font-weight: 700;">
+                      <?php if($user_follow) { ?>
+                        Following 
+                      <?php } else {  ?>  
+                          Follow
+                        <?php }  ?> 
+                      </button>
+                    </div>
+                  </div>
+
+                  <?php }?>
+         
+          
+          </div>
+  
+  
+        </div>
+      </div>
+      </div> 
+      <script src="assets/js/search.js"></script>
+          <script src="assets/js/photo.js?v=<?php echo time(); ?>"></script>
+          <script type="text/javascript" src="assets/js/hashtag.js"></script>
+          <script type="text/javascript" src="assets/js/like.js"></script>
+          <script type="text/javascript" src="assets/js/comment.js?v=<?php echo time(); ?>"></script>
+          <script type="text/javascript" src="assets/js/retweet.js?v=<?php echo time(); ?>"></script>
+          <script type="text/javascript" src="assets/js/follow.js?v=<?php echo time(); ?>"></script>
+      <script src="https://kit.fontawesome.com/38e12cc51b.js" crossorigin="anonymous"></script>
+      <!-- <script src="assets/js/jquery-3.4.1.slim.min.js"></script> -->
+      <script src="assets/js/jquery-3.5.1.min.js"></script>
+
+        <script src="assets/js/popper.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html> 
