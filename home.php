b1390c53af817bf48133078de6437fb3966a75ef
excellentservantacademy <excellentservantacademy@gmail.com>
2025-09-30 23:06:06 +0200

    added the header'

--- home.php
+++ home.php
@@ -15,4 +15,18 @@
    $notify_count = User::CountNotification($user_id);
  
 ?>
-  
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | Twitter Clone</title>
+    
+    <link rel="shortcut icon" type="image/png" href="assets/images/twitter.svg"> 
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/all.min.css">
+        <link rel="stylesheet" href="assets/css/home_style.css?v=<?php echo time(); ?>">
+    
+   
+</head>
+<body>
