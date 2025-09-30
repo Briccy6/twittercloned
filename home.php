9582aed757b3ded15e82053517e82404229664a2
excellentservantacademy <excellentservantacademy@gmail.com>
2025-09-30 23:04:10 +0200

    start of home page

---
+++ home.php
@@ -0,0 +1,18 @@
+<?php
+
+   include 'core/init.php';
+  
+   $user_id = $_SESSION['user_id'];
+
+   $user = User::getData($user_id);
+   
+   if (User::checkLogIn() === false) 
+   header('location: index.php');
+
+
+   $tweets = Tweet::tweets($user_id);
+   $who_users = Follow::whoToFollow($user_id);
+   $notify_count = User::CountNotification($user_id);
+ 
+?>
+  
