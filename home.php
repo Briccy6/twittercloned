a2376d44e86e295ae4b5a1ea6e164bbbb6151ed6
excellentservantacademy <excellentservantacademy@gmail.com>
2025-09-30 23:11:29 +0200

    added some

--- home.php
+++ home.php
@@ -111,4 +111,165 @@
                 width="26.25px"
               />
             </div>
+     <div class="wrapper-left-elements">
+              <a href="notification.php" style="margin-top: 4px"><strong>Notifications</strong></a>
+            </div>
+          </div>
+          </a>
+        
+            <a href="<?php echo BASE_URL . $user->username; ?>">
+          <div class="grid-sidebar">
+            <div class="icon-sidebar-align">
+              <img src="<?php echo BASE_URL . "/includes/icons/tweetprof.png"; ?>" alt="" height="26.25px" width="26.25px" />
+            </div>
   
+            <div class="wrapper-left-elements">
+              <!-- <a href="/twitter/<?php echo $user->username; ?>"  style="margin-top: 4px"><strong>Profile</strong></a> -->
+              <a  href="<?php echo BASE_URL . $user->username; ?>"  style="margin-top: 4px"><strong>Profile</strong></a>
+            
+            </div>
+          </div>
+          </a>
+          <a href="<?php echo BASE_URL . "account.php"; ?>">
+          <div class="grid-sidebar ">
+            <div class="icon-sidebar-align">
+              <img src="<?php echo BASE_URL . "/includes/icons/tweetsetting.png"; ?>" alt="" height="26.25px" width="26.25px" />
+            </div>
+  
+            <div class="wrapper-left-elements">
+              <a href="<?php echo BASE_URL . "account.php"; ?>" style="margin-top: 4px"><strong>Settings</strong></a>
+            </div>
+           
+            
+          </div>
+          </a>
+          <a href="includes/logout.php">
+          <div class="grid-sidebar">
+            <div class="icon-sidebar-align">
+            <i style="font-size: 26px;" class="fas fa-sign-out-alt"></i>
+            </div>
+  
+            <div class="wrapper-left-elements">
+              <a href="includes/logout.php" style="margin-top: 4px"><strong>Logout</strong></a>
+            </div>
+          </div>
+          </a>
+          <button class="button-twittear">
+            <strong>Tweet</strong>
+          </button>
+  
+          <div class="box-user">
+            <div class="grid-user">
+              <div>
+                <img
+                  src="assets/images/users/<?php echo $user->img ?>"
+                  alt="user"
+                  class="img-user"
+                />
+              </div>
+              <div>
+                <p class="name"><strong><?php if($user->name !== null) {
+                echo $user->name; } ?></strong></p>
+                <p class="username">@<?php echo $user->username; ?></p>
+              </div>
+              <div class="mt-arrow">
+                <img
+                  src="https://i.ibb.co/mRLLwdW/arrow-down.png"
+                  alt=""
+                  height="18.75px"
+                  width="18.75px"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+          
+
+      <div class="grid-posts">
+        <div class="border-right">
+          <div class="grid-toolbar-center">
+            <div class="center-input-search">
+              <div class="input-group-login" id="whathappen">
+                
+                <div class="container">
+                  <div class="part-1">
+                    <div class="header">
+                      <div class="home">
+                        <h2>Home</h2>
+                      </div>
+                      <!-- <div class="icon">
+                        <button type="button" name="button">+</button>
+                      </div> -->
+                    </div>
+            
+                    <div class="text">
+                      <form class="" action="handle/handleTweet.php" method="post" enctype="multipart/form-data">
+                        <div class="inner">
+            
+                            <img src="assets/images/users/<?php echo $user->img ?>" alt="profile photo">
+                        
+                          <label>
+            
+                            <textarea class="text-whathappen" name="status" rows="8" cols="80" placeholder="What's happening?"></textarea>
+                        
+                        </label>
+                        </div> 
+                            
+                         <!-- tmp image upload place -->
+                        <div class="position-relative upload-photo"> 
+                          <img class="img-upload-tmp" src="assets/images/tweets/tweet-60666d6b426a1.jpg" alt="">
+                          <div class="icon-bg">
+                          <i id="#upload-delete-tmp" class="fas fa-times position-absolute upload-delete"></i>  
+
+                          </div>
+                        </div>
+
+
+                        <div class="bottom"> 
+                          
+                          <div class="bottom-container">
+                              
+                            
+                              
+                           
+                            <label for="tweet_img" class="ml-3 mb-2 uni">
+
+                              <i class="fa fa-image item1-pair"></i>
+                            </label>
+                            <input class="tweet_img" id="tweet_img" type="file" name="tweet_img">    
+                                
+                          </div>
+                          <div class="hash-box">
+                        
+                              <ul style="margin-bottom: 0;">
+
+
+                              </ul>
+                          
+                          </div>
+                          <?php if (isset($_SESSION['errors_tweet'])) { 
+                            
+                            foreach($_SESSION['errors_tweet'] as $t) {?>
+                            
+                          <div class="alert alert-danger">
+                          <span class="item2-pair"> <?php echo $t; ?> </span>
+                          </div>
+                         
+                         <?php } } unset($_SESSION['errors_tweet']); ?>
+                          <div>
+                         
+                            <span class="bioCount" id="count">140</span>
+                            <input id="tweet-input" type="submit" name="tweet" value="Tweet" class="submit"
+                            >
+                          </div>
+                      </div>
+                      </form>
+                    </div>
+                  </div>
+                  <div class="part-2">
+            
+                  </div>
+            
+                </div>
+                
