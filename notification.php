fcf8821e7c1becda0ceea2a99a81c0df69c0383a
excellentservantacademy <excellentservantacademy@gmail.com>
2025-09-30 23:18:22 +0200

    iii'

--- notification.php
+++ notification.php
@@ -16,3 +16,17 @@
             header('location: index.php');    
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notifications | Twitter Clone</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/profile_style.css?v=<?php echo time(); ?>">
+  
+    <link rel="shortcut icon" type="image/png" href="assets/images/twitter.svg"> 
+   
+</head>
+<body>
